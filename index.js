a51709f4e3d9a261645f67e75e187000446b3bb4
Jose Fernández <48722287+joseedaniel@users.noreply.github.com>
2023-04-21 20:58:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+    alert('Yo te amo más uwu')
 });
 
 const noBtn = document.querySelector('#noBtn');
